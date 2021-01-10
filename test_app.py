cd8ba1ba93d4789402dd795dfdc779d6f73e1a91
Oleg Kozlov <xxblx@posteo.org>
2021-01-10 12:11:35 +0000

    update tests

--- test_app.py
+++ test_app.py
@@ -4,25 +4,27 @@ from urllib.parse import urljoin, urlencode
 from uuid import uuid4
 
 import tornado.ioloop
-import keyring
 import pytest
 
+from db_manage import create_user, delete_user
 from server.app import ServerApp, get_db_pool
 
-USERNAME = 'oleg'
-PASSWORD = keyring.get_password('tasker-dev', USERNAME)
-
-
-async def get_tokens(http_client, base_url, password=None):
-    data = {'username': USERNAME, 'password': PASSWORD}
-    if password is not None:
-        data['password'] = password
-    return await http_client.fetch(
-        urljoin(base_url, '/api/tokens/new'),
-        method='POST',
-        body=urlencode(data),
-        raise_error=False
-    )
+USERNAMES = [uuid4().hex for i in range(3)]
+USERS = [
+    {'username': u, 'password': create_user(u, generate_password=True)}
+    for u in USERNAMES
+]
+ENDPOINTS = {
+    'new_tokens': '/api/tokens/new',
+    'renew_tokens': '/api/tokens/renew',
+    'project_base': '/api/project',
+    'project': '/api/project/{}',
+    'folder_project': '/api/folder/{}',
+    'folder': '/api/folder/{}/{}',
+    'task_project': '/api/project/{}',
+    'task_folder': '/api/task/{}/{}',
+    'task': '/api/task/{}/{}/{}'
+}
 
 
 @pytest.fixture
@@ -33,20 +35,34 @@ def app():
 
 
 @pytest.mark.gen_test
-async def test_api_tokens_get(http_client, base_url):
+async def test_api_tokens_new(http_client, base_url):
+    user = USERS[2].copy()
     # Valid username and password
     r = await http_client.fetch(
-        urljoin(base_url, '/api/tokens/new'),
+        urljoin(base_url, ENDPOINTS['new_tokens']),
         method='POST',
-        body=urlencode({'username': USERNAME, 'password': PASSWORD})
+        body=urlencode(user)
     )
     assert r.code == 200
 
-    # Invalid password
+    # Valid username and invalid password
+    user['password'] = uuid4().hex
     r = await http_client.fetch(
-        urljoin(base_url, '/api/tokens/new'),
+        urljoin(base_url, ENDPOINTS['new_tokens']),
         method='POST',
-        body=urlencode({'username': USERNAME, 'password': 'PASSWORD'}),
+        body=urlencode(user),
+        raise_error=False
+    )
+    assert r.code == 403
+
+    # Delete user and try get new tokens again
+    # Invalid username and invalid password
+    user = USERS[2]
+    delete_user(user['username'])
+    r = await http_client.fetch(
+        urljoin(base_url, ENDPOINTS['new_tokens']),
+        method='POST',
+        body=urlencode(user),
         raise_error=False
     )
     assert r.code == 403
@@ -54,10 +70,11 @@ async def test_api_tokens_get(http_client, base_url):
 
 @pytest.mark.gen_test
 async def test_api_tokens_renew(http_client, base_url):
+    user = USERS[1]
     r = await http_client.fetch(
-        urljoin(base_url, '/api/tokens/new'),
+        urljoin(base_url, ENDPOINTS['new_tokens']),
         method='POST',
-        body=urlencode({'username': USERNAME, 'password': PASSWORD})
+        body=urlencode(user)
     )
     tokens = json.loads(r.body)
     del tokens['expires_in']
@@ -66,7 +83,7 @@ async def test_api_tokens_renew(http_client, base_url):
     tokens_invalid = tokens.copy()
     tokens_invalid['token_renew'] = uuid4().hex
     r = await http_client.fetch(
-        urljoin(base_url, '/api/tokens/renew'),
+        urljoin(base_url, ENDPOINTS['renew_tokens']),
         method='POST',
         body=urlencode(tokens_invalid),
         raise_error=False
@@ -75,15 +92,28 @@ async def test_api_tokens_renew(http_client, base_url):
 
     # Valid renew token
     r = await http_client.fetch(
-        urljoin(base_url, '/api/tokens/renew'),
+        urljoin(base_url, ENDPOINTS['renew_tokens']),
         method='POST',
         body=urlencode(tokens)
     )
     assert r.code == 200
+    tokens_outdated = tokens
+    tokens = json.loads(r.body)
+    del tokens['expires_in']
 
     # Try to use outdated tokens
     r = await http_client.fetch(
-        urljoin(base_url, '/api/tokens/renew'),
+        urljoin(base_url, ENDPOINTS['renew_tokens']),
+        method='POST',
+        body=urlencode(tokens_outdated),
+        raise_error=False
+    )
+    assert r.code == 403
+
+    # Delete user and try to renew tokens again
+    delete_user(user['username'])
+    r = await http_client.fetch(
+        urljoin(base_url, ENDPOINTS['renew_tokens']),
         method='POST',
         body=urlencode(tokens),
         raise_error=False
