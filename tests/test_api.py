cc28c601f6e6cd4b0daa8b5fdfdd65a5c2a9e154
Oleg Kozlov <xxblx@posteo.org>
2021-01-10 17:58:17 +0000

    Improved API tests + test getting projects list
    - Moved data fetching moved to separate function `fetch`
    - Fixture with creating a temporary user for a test case
    - New test - get projects list by user

--- tests/test_api.py
+++ tests/test_api.py
@@ -3,21 +3,19 @@ import sys
 import json
 import os.path
 from urllib.parse import urljoin, urlencode
+from datetime import datetime
+from time import mktime
 from uuid import uuid4
 
 import tornado.ioloop
 import pytest
+from tornado.httputil import url_concat
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 from db_manage import create_user, delete_user
 from server.app import ServerApp, get_db_pool
 
-USERNAMES = [uuid4().hex for i in range(3)]
-USERS = [
-    {'username': u, 'password': create_user(u, generate_password=True)}
-    for u in USERNAMES
-]
-ENDPOINTS = {
+URI = {
     'new_tokens': '/api/tokens/new',
     'renew_tokens': '/api/tokens/renew',
     'project_base': '/api/project',
@@ -30,6 +28,33 @@ ENDPOINTS = {
 }
 
 
+async def fetch(http_client, base_url, uri, method='POST', params=None):
+    parameters = {'method': method, 'raise_error': False}
+    if method == 'GET':
+        parameters['request'] = url_concat(urljoin(base_url, uri), params)
+    elif method == 'POST':
+        parameters['request'] = urljoin(base_url, uri)
+        parameters['body'] = urlencode(params)
+    return await http_client.fetch(**parameters)
+
+
+async def get_new_tokens(http_client, base_url, user):
+    r = await fetch(http_client, base_url, URI['new_tokens'], 'POST', user)
+    data = json.loads(r.body)
+    return {k: data[k] for k in ('token_select', 'token_verify')}
+
+
+@pytest.fixture
+def user():
+    username = 'user_{}'.format(int(mktime(datetime.utcnow().timetuple())))
+    user_dict = {
+        'username': username,
+        'password': create_user(username, generate_password=True)
+    }
+    yield user_dict
+    delete_user(username)
+
+
 @pytest.fixture
 def app():
     loop = tornado.ioloop.IOLoop.current()
@@ -38,87 +63,62 @@ def app():
 
 
 @pytest.mark.gen_test
-async def test_api_tokens_new(http_client, base_url):
-    user = USERS[2].copy()
+async def test_tokens_new(http_client, base_url, user):
     # Valid username and password
-    r = await http_client.fetch(
-        urljoin(base_url, ENDPOINTS['new_tokens']),
-        method='POST',
-        body=urlencode(user)
-    )
+    r = await fetch(http_client, base_url, URI['new_tokens'], 'POST', user)
     assert r.code == 200
 
     # Valid username and invalid password
+    password = user['password']
     user['password'] = uuid4().hex
-    r = await http_client.fetch(
-        urljoin(base_url, ENDPOINTS['new_tokens']),
-        method='POST',
-        body=urlencode(user),
-        raise_error=False
-    )
+    r = await fetch(http_client, base_url, URI['new_tokens'], 'POST', user)
     assert r.code == 403
 
-    # Delete user and try get new tokens again
+    # Delete user and try to get new tokens again
     # Invalid username and invalid password
-    user = USERS[2]
+    user['password'] = password
     delete_user(user['username'])
-    r = await http_client.fetch(
-        urljoin(base_url, ENDPOINTS['new_tokens']),
-        method='POST',
-        body=urlencode(user),
-        raise_error=False
-    )
+    r = await fetch(http_client, base_url, URI['new_tokens'], 'POST', user)
     assert r.code == 403
 
 
 @pytest.mark.gen_test
-async def test_api_tokens_renew(http_client, base_url):
-    user = USERS[1]
-    r = await http_client.fetch(
-        urljoin(base_url, ENDPOINTS['new_tokens']),
-        method='POST',
-        body=urlencode(user)
-    )
+async def test_tokens_renew(http_client, base_url, user):
+    r = await fetch(http_client, base_url, URI['new_tokens'], 'POST', user)
     tokens = json.loads(r.body)
     del tokens['expires_in']
 
-    # Invalid renew token
-    tokens_invalid = tokens.copy()
-    tokens_invalid['token_renew'] = uuid4().hex
-    r = await http_client.fetch(
-        urljoin(base_url, ENDPOINTS['renew_tokens']),
-        method='POST',
-        body=urlencode(tokens_invalid),
-        raise_error=False
-    )
-    assert r.code == 403
-
     # Valid renew token
-    r = await http_client.fetch(
-        urljoin(base_url, ENDPOINTS['renew_tokens']),
-        method='POST',
-        body=urlencode(tokens)
-    )
+    r = await fetch(http_client, base_url, URI['renew_tokens'], 'POST', tokens)
     assert r.code == 200
     tokens_outdated = tokens
     tokens = json.loads(r.body)
     del tokens['expires_in']
 
     # Try to use outdated tokens
-    r = await http_client.fetch(
-        urljoin(base_url, ENDPOINTS['renew_tokens']),
-        method='POST',
-        body=urlencode(tokens_outdated),
-        raise_error=False
-    )
+    r = await fetch(http_client, base_url, URI['renew_tokens'], 'POST',
+                    tokens_outdated)
+    assert r.code == 403
+
+    # Invalid renew token for valid select token and verify tokens
+    tokens_invalid_renew = tokens.copy()
+    tokens_invalid_renew['token_renew'] = uuid4().hex
+    r = await fetch(http_client, base_url, URI['renew_tokens'], 'POST',
+                    tokens_invalid_renew)
     assert r.code == 403
 
     # Delete user and try to renew tokens again
     delete_user(user['username'])
-    r = await http_client.fetch(
-        urljoin(base_url, ENDPOINTS['renew_tokens']),
-        method='POST',
-        body=urlencode(tokens),
-        raise_error=False
-    )
+    r = await fetch(http_client, base_url, URI['renew_tokens'], 'POST', tokens)
     assert r.code == 403
+
+
+@pytest.mark.gen_test
+async def test_project_list(http_client, base_url, user):
+    tokens = await get_new_tokens(http_client, base_url, user)
+    r = await fetch(http_client, base_url, URI['project_base'], 'GET', tokens)
+    assert r.code == 200
+    data = json.loads(r.body)
+    assert len(data['projects']) == 1
+    keys = list(data['projects'][0].keys())
+    assert keys == ['id', 'role', 'title', 'description']
